3e8def356881173a5e1de9a6590959dce4e2db45
Calista Besseling <calista.besseling@gmail.com>
2025-05-24 11:46:47 -0400

    initial commit

---
+++ Content.js
@@ -0,0 +1,9 @@
+const generateHTML = (platform) => {
+    return `
+    GET BACK TO WORK
+     `;
+  };
+  
+if (window.location.hostname === "www.instagram.com"){
+    document.body.innerHTML = generateHTML('instagram')
+}
