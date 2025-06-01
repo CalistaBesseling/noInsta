93ee3cfc6adc1f84dfca5f5ca6f5f6fea53ef4af
Calista Besseling <calista.besseling@gmail.com>
2025-06-01 12:18:35 -0400

    Redirects to leetcode, if submission is undefined

--- Content.js
+++ Content.js
@@ -1,9 +1,28 @@
-const generateHTML = (platform) => {
-    return `
-    GET BACK TO WORK
-     `;
-  };
-  
-if (window.location.hostname === "www.instagram.com"){
-    document.body.innerHTML = generateHTML('instagram')
-}
+// get todays date
+const today = new Date();
+today.setUTCHours(0, 0, 0, 0);
+const time = Math.floor(today.getTime() / 1000);
+console.log("time:" + time);
+
+(async () => {
+    // get leetcode submissions for today
+    try{
+        const response = await fetch("https://alfa-leetcode-api.onrender.com/Calista_Besseling/calendar");
+        const outer = await response.json(); 
+        const inner = JSON.parse(outer.submissionCalendar);
+
+        const submissionsToday = inner[time];
+        console.log(submissionsToday)
+
+        // if 0 or undefined, redirect
+        if (!submissionsToday){
+            console.log("noooooo no instagram no!");
+            window.location.replace("https://leetcode.com/problemset/");
+        } else{
+            console.log("yay! Instagram!");
+        }
+
+    }catch(err){
+        console.error("Uh oh! ", err);
+    }
+})();
